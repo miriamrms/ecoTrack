af5c1a41b576954f9685400140aba89ba0b420c1
Miriam Mendes <miriam.rayanemendes@ufpe.br>
2024-10-23 11:05:01 -0300

    add xCode file

---
+++ ecoTrack/ecoTrack/ecoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoTrackApp.swift
+//  ecoTrack
+//
+//  Created by Miriam Mendes on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ecoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
