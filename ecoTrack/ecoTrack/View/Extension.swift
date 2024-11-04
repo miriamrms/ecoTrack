6621177f9f701ce88d716a68ba033bbd1a37e6e0
Miriam Mendes <miriam.rayanemendes@ufpe.br>
2024-11-04 12:04:55 -0300

    creat goals component

---
+++ ecoTrack/ecoTrack/View/Extension.swift
@@ -0,0 +1,47 @@
+//
+//  Extension.swift
+//  ecoTrack
+//
+//  Created by Miriam Mendes on 04/11/24.
+//
+
+import SwiftUI
+
+
+struct RoundedCorner: Shape {
+    let radius: CGFloat
+    let corners: UIRectCorner
+
+    init(radius: CGFloat = .infinity, corners: UIRectCorner = .allCorners) {
+        self.radius = radius
+        self.corners = corners
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+
+extension Shape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .stroke(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
+
+extension InsettableShape {
+    func fill<Fill: ShapeStyle, Stroke: ShapeStyle>(_ fillStyle: Fill, strokeBorder strokeStyle: Stroke, lineWidth: Double = 1) -> some View {
+        self
+            .strokeBorder(strokeStyle, lineWidth: lineWidth)
+            .background(self.fill(fillStyle))
+    }
+}
--- ecoTrack/ecoTrack/View/ResourceView.swift
+++ ecoTrack/ecoTrack/View/ResourceView.swift
@@ -14,116 +14,134 @@ struct ResourceView: View {
 
     
     var body: some View {
-        VStack(spacing: 40){
-            ZStack{
-                resource.cor[1] //Cor do Recurso
-                Image(resource.bgImage)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(maxWidth: width)
-                
-                VStack(){
-                    HStack{
-                        Image(resource.singleIcon)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxHeight: 27)
-                        Text(resource.rawValue)
-                    }
-                    .padding(.top, height * 0.08)
-                    .font(.system(size: 19, weight: .bold))
-                    Spacer()
-                    HStack{
-                        VStack(alignment: .leading){
-                            HStack(alignment: .bottom){
-                                Image("bar-chart-2")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(maxHeight: 27)
-                                Text("Média Mensal")
+        ScrollView(){
+            VStack(spacing: 40){
+                ZStack{
+                    resource.cor[1] //Cor do Recurso
+                    Image(resource.bgImage)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: width)
+                    
+                    VStack(){
+                        HStack{
+                            Image(resource.singleIcon)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxHeight: 27)
+                            Text(resource.rawValue)
+                        }
+                        .padding(.top, height * 0.08)
+                        .font(.system(size: 19, weight: .bold))
+                        Spacer()
+                        HStack{
+                            VStack(alignment: .leading){
+                                HStack(alignment: .bottom){
+                                    Image("bar-chart-2")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxHeight: 27)
+                                    Text("Média Mensal")
+                                }
+                                .font(.system(size: 16, weight: .regular))
+                                Divider()
+                                    .overlay(Color.white)
+                                HStack{
+                                    Image("ruler")
+                                    Text("1652 L")
+                                }
+                                .font(.system(size: 23, weight: .bold))
+                                HStack{
+                                    Image("coin")
+                                    Text("R$657,00")
+                                }
+                                .font(.system(size: 23, weight: .bold))
+                                
+                            }
+                            .frame(width: width*0.40)
+                            Spacer()
+                            VStack{
+                                Button {
+                                    //
+                                } label: {
+                                    ZStack{
+                                        HStack{
+                                            Image(systemName: "clock.arrow.trianglehead.counterclockwise.rotate.90")
+                                                .font(.system(size: 18, weight: .bold))
+                                            Text("Histórico")
+                                        }
+                                        .foregroundColor(resource.cor[1])
+                                        .padding(.top, 14)
+                                        .padding(.bottom, 14)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.white)
+                                    .cornerRadius(8)
+                                }
+                                Button {
+                                    //
+                                } label: {
+                                    ZStack{
+                                        VStack{
+                                            Image(systemName: "plus.app")
+                                                .font(.system(size: 18, weight: .bold))
+                                            Text("Adicionar Gasto Mensal")
+                                        }
+                                        .foregroundColor(resource.cor[1])
+                                        .padding(.top, 14)
+                                        .padding(.bottom, 14)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color.white)
+                                    .cornerRadius(8)
+                                }
+
                             }
                             .font(.system(size: 16, weight: .regular))
-                            Divider()
-                                .overlay(Color.white)
-                            HStack{
-                                Image("ruler")
-                                Text("1652 L")
-                            }
-                            .font(.system(size: 23, weight: .bold))
-                            HStack{
-                                Image("coin")
-                                Text("R$657,00")
-                            }
-                            .font(.system(size: 23, weight: .bold))
-                            
+                            .frame(width: width*0.34)
                         }
-                        .frame(width: width*0.40)
-                        Spacer()
-                        VStack{
-                            Button {
-                                //
-                            } label: {
-                                ZStack{
-                                    HStack{
-                                        Image(systemName: "clock.arrow.trianglehead.counterclockwise.rotate.90")
-                                            .font(.system(size: 18, weight: .bold))
-                                        Text("Histórico")
-                                    }
-                                    .foregroundColor(resource.cor[1])
-                                    .padding(.top, 14)
-                                    .padding(.bottom, 14)
-                                }
-                                .frame(maxWidth: .infinity)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                            }
-                            Button {
-                                //
-                            } label: {
-                                ZStack{
-                                    VStack{
-                                        Image(systemName: "plus.app")
-                                            .font(.system(size: 18, weight: .bold))
-                                        Text("Adicionar Gasto Mensal")
-                                    }
-                                    .foregroundColor(resource.cor[1])
-                                    .padding(.top, 14)
-                                    .padding(.bottom, 14)
-                                }
-                                .frame(maxWidth: .infinity)
-                                .background(Color.white)
-                                .cornerRadius(8)
-                            }
-
-                        }
-                        .font(.system(size: 16, weight: .regular))
-                        .frame(width: width*0.34)
+                        .padding(.bottom, 20)
                     }
-                    .padding(.bottom, 20)
+                    .padding(20)
+                    
+                    
                 }
-                .padding(20)
+                .ignoresSafeArea()
+                .frame(maxHeight: 299)
+                .foregroundColor(Color.white)
+                .frame(alignment: .top)
                 
-                
-            }
-            .ignoresSafeArea()
-            .frame(maxHeight: 299)
-            .foregroundColor(Color.white)
-            .frame(alignment: .top)
-            
-            ScrollView(.horizontal){
-                HStack{
-                    ForEach(Analysis.allCases, id: \.self){ analysis in
-                        AnalysisButtonView(analysis: analysis, resource: resource, valor: 0.3)
+                ScrollView(.horizontal){
+                    HStack{
+                        ForEach(Analysis.allCases, id: \.self){ analysis in
+                            AnalysisButtonView(analysis: analysis, resource: resource, valor: 0.3)
+                        }
                     }
                 }
-            }
-            VStack{
-                
+                VStack(alignment: .leading){
+                    HStack{
+                        Image("greenLeaf")
+                        Text("Metas sustentáveis")
+        
+                    }
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundStyle(Color.verdeClaro)
+                    .padding(.leading, 15)
+                    
+                    VStack(spacing: 12){
+                        ResourceGoalsView(resource: resource)
+                        ResourceGoalsView(resource: resource)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
             }
         }
+        .ignoresSafeArea()
+        
     }
 }
 
 #Preview {
-    ResourceView(resource: .agua)
+    ResourceView(resource: .energia)
 }
